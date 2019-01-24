c9ca3e6a968ee76022cca9ba216c46de2fe80175
Bruno <you@example.com>
2019-01-24 00:28:21 -0300

    Adicionamento da classe Veiculo

--- veiculos/models.py
+++ veiculos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Veiculo(models.Model):
+      tipo = models.Charfield(max_length=50)
+      nome = models.Charfield(max_length=50)
+      ano = models.Integerfield(max_length=4)
+      fabricante = models.Charfield(max_length=50)
+
+
+
